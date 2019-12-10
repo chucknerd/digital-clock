2d58375abd5f524794da22d98f2a8ccb3d7424f6
chucknerd <chucknerd1@gmail.com>
2019-12-10 06:01:13 -0600

    javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("hours");
+const hours = new Date(hours);
+getHours();
+
+document.getElementById("minutes");
+const minutes = new Date(minutes);
+getMinutes();
+
+document.getElementById("seconds");
+const seconds = new Date(seconds);
+getSeconds();
