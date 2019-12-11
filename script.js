355dd4cb0ed652e2ae1f778787662e9a2aa208e1
chucknerd <chucknerd1@gmail.com>
2019-12-11 05:59:16 -0600

    FINAL PUSH

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-document.getElementById("hours");
-const hours = new Date(hours);
-getHours();
+// tutorial: https://www.youtube.com/watch?v=mnAqcmVHONY&t=436s
 
-document.getElementById("minutes");
-const minutes = new Date(minutes);
-getMinutes();
+var time = new Date();
+console.log(time.getHours() % 12);
+console.log(time.getMinutes());
+console.log(time.getSeconds());
 
-document.getElementById("seconds");
-const seconds = new Date(seconds);
-getSeconds();
+var clock = document.getElementById("clock");
+var hexColor = document.getElementById("hex-color");
+
+function hexClock() {
+    var time = new Date();
+    var hours = (time.getHours() %12).toString();
+    var minutes = time.getMinutes().toString();
+    var seconds = time.getSeconds().toString();
+
+    if (hours.length < 2) {
+        hours = "0" + hours;
+    }
+
+    if (minutes.length < 2) {
+        minutes = "0" + minutes;
+    }
+
+    if (seconds.length < 2) {
+        seconds = "0" + seconds;
+    }
+
+    var clockStr= hours + ":" + minutes + "." + seconds;
+    var hexColorStr = "#" + hours + minutes + seconds;
+
+    clock.textContent = clockStr;
+    hexColor.textContent = hexColorStr;
+
+    document.body.style.backgroundColor = hexColorStr;
+}
+
+hexClock();
+setInterval(hexClock, 1000);
